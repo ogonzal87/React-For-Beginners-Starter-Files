ca41317852f147f1d53b882279a8c01794843a9a
Oscar Gonzalez <ogonzal87@gmail.com>
2018-09-28 17:37:33 -0700

    taking out the commas

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
-import sampleFishes from '../sample-fishes'
-import Fish from './Fish'
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
     fishes: {},
-    order: {},
-  }
+    order: {}
+  };
 
-  addFish = (fish) => {
+  addFish = fish => {
     // 1. Take a copy of the existing state
-    const fishes = { ...this.state.fishes }
+    const fishes = { ...this.state.fishes };
     // 2. Add our new fish to that fishes
-    fishes[`fish${Date.now()}`] = fish
+    fishes[`fish${Date.now()}`] = fish;
     // 3. Set the new fishes object to state
-    this.setState({ fishes })
-  }
+    this.setState({ fishes });
+  };
 
   loadSamples = () => {
-    this.setState({ fishes: sampleFishes })
-  }
+    this.setState({ fishes: sampleFishes });
+  };
 
-  addToOrder = (key) => {
+  addToOrder = key => {
     // 1. Take a copy of state
-    const order = { ...this.state.order }
+    const order = { ...this.state.order };
     // 2. Either add to the order or update the number of items ordered
-    order[key] = order[key] + 1 || 1
+    order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
-    this.setState({ order })
-  }
+    this.setState({ order });
+  };
 
   render() {
     return (
@@ -48,11 +48,10 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} orders={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
